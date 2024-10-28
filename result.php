e24028c0b0847d19eedb3c4f6f00b4268faf7157
sandy0v0 <be0324ss@gmail.com>
2024-10-28 13:57:42 +0800

    示範透過連結傳遞資料

--- result.php
+++ result.php
@@ -5,14 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BMI結果</title>
 </head>
+<?php
+if(isset($_GET['height'])){
+    $height=$_GET['height'];
+}else if(isset($_POST['height'])){
+    $height=$_POST['height'];
 
+}else{
+    echo "請使用正確管道進到此頁面!";
+    exit();
+}
+
+if(isset($_GET['weight'])){
+    $weight=$_GET['weight'];
+}else if(isset($_POST['weight'])){
+    $weight=$_POST['weight'];
+
+}else{
+    echo "請使用正確管道進到此頁面!";
+    exit();
+}
+
+?>
 <h1>BMI結果</h1>
+<!-- 
 <div>你的身高:<?=$_GET['height'];?>公分</div>
-<div>你的體重:<?=$_GET['weight'];?>公斤</div>
-<?php 
-$h=$_GET['height']/100;
+<div>你的體重:<?=$_GET['weight'];?>公斤</div> -->
 
-$bmi=round($_GET['weight']/($h * $h),2);
+<div>你的身高:<?=$height;?>公分</div>
+<div>你的體重:<?=$weight;?>公斤</div>
+
+<?php 
+// $h=$_GET['height']/100;
+$h=$height/100;
+
+// $bmi=round($_GET['weight']/($h * $h),2);
+$bmi=round($weight/($h * $h),2);
 
 if($bmi<18.5){
     $level="體重過輕";
@@ -33,7 +61,8 @@ if($bmi<18.5){
 <div>你的BMI為:<?=$bmi;?></div>
 <div>體位判定為:<?=$level;?></div>
 <div>
-    <a href="index.php">重首頁/重新量測</a>
+    <!-- ?bmi=<?=$bmi;?>代表，如果第二次使用者，會顯示前一次的BMI值 -->
+    <a href="index.php?bmi=<?=$bmi;?>">重首頁/重新量測</a>
 </div>
 
 </body>
