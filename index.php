d0e31e9e89c6d48d5fbac87339603353c09e5b1b
sandy0v0 <be0324ss@gmail.com>
2024-10-28 13:09:57 +0800

    進行體位判定

--- index.php
+++ index.php
@@ -6,21 +6,20 @@
     <title>計算BMI</title>
 </head>
 <body>
-
     <h1>計算BMI</h1>
-    <form action="result.php" method="get">
-        <div>
-            <label for="height">身高：</label>
-            <input type="number" name="height" id="height">/cm
-        </div>
-        <div>
-            <label for="weight">體重：</label>
-            <input type="number" name="weight" id="weight">/kg
-        </div>
-        <div>
-            <input type="submit" value="計算">
-            <input type="reset" value="清空/重置">
-        </div>
+        <form action="result.php" method="get">
+            <div>
+                <label for="height">身高：</label>
+                <input type="number" name="height" id="height" step="0.1" >/cm
+            </div>
+            <div>
+                <label for="weight">體重：</label>
+                <input type="number" name="weight" id="weight" step="0.1">/kg
+            </div>
+            <div>
+                <input type="submit" value="計算">
+                <input type="reset" value="清空/重置">
+            </div>
     </form>
 </body>
 </html>
--- result.php
+++ result.php
@@ -5,24 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BMI結果</title>
 </head>
-<body>
 
-    <h1>BMI結果</h1>    
-    <div>你的身高:<?=$_GET['height'];?>公分</div>
-    <div>你的體重:<?=$_GET['weight'];?>公斤</div>
-    <?php
-    $h=$_GET['height']/100;
+<h1>BMI結果</h1>
+<div>你的身高:<?=$_GET['height'];?>公分</div>
+<div>你的體重:<?=$_GET['weight'];?>公斤</div>
+<?php 
+$h=$_GET['height']/100;
 
-    $bmi=round($_GET['weight']/($h * $h),2);
+$bmi=round($_GET['weight']/($h * $h),2);
+
+if($bmi<18.5){
+    $level="體重過輕";
+}else if($bmi<24){
+    $level="健康體位";
+}else if($bmi<27){
+    $level="過重";
+}else if($bmi<30){
+    $level="輕度肥胖";
+}else if($bmi<35){
+    $level="中度肥胖";
+}else{
+    $level="重度肥胖";
+}
 
 
-    ?>
-
-    <div>你的BMI為:<?=$bmi;?></div>
-    <div>體位判定為:</div>
-    <div>
-        <a href="index.php">重首頁/重新量測</a>
-    </div>
+?>
+<div>你的BMI為:<?=$bmi;?></div>
+<div>體位判定為:<?=$level;?></div>
+<div>
+    <a href="index.php">重首頁/重新量測</a>
+</div>
 
 </body>
 </html>
