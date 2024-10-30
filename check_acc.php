65f00b451deba45c20a83b51cb5bab192964d584
sandy0v0 <be0324ss@gmail.com>
2024-10-30 13:06:34 +0800

    使用get傳遞參數的方式來控制首頁內容

--- check_acc.php
+++ check_acc.php
@@ -1,10 +1,19 @@
 <?php 
+session_start();
+if(!isset($_GET['login'])){
+    header("location:login.php");
+    exit();
+}
+
 
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
 if($acc=='admin' && $pw=='123456'){
     echo "帳密正確：登入成功";
+    session_start();
+    $_SESSION['login']=$acc;
+    echo "<br><a href='login.php?login=$acc'>回首頁</a>";
 }else{
     echo "帳密錯誤：登入失敗";
 
