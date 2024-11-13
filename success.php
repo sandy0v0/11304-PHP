f6a14da94ab77868741279fad1cbf0b513001872
sandy0v0 <be0324ss@gmail.com>
2024-11-13 15:43:39 +0800

    完成刪除資料功能

--- success.php
+++ success.php
@@ -7,5 +7,56 @@
 </head>
 <body>
     <h1>登入成功</h1>
+
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+$pdo=new PDO($dsn,'root','');
+
+$rows=$pdo->query("select * from member")->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<table>
+    <tr>
+        <td>id</td>
+        <td>acc</td>
+        <td>pw</td>
+        <td>email</td>
+        <td>tel</td>
+        <td>操作</td>
+    </tr>
+
+<?php
+foreach($rows as $row){
+
+?> 
+<tr>
+        <td><?=$row['id'];?></td>
+        <td><?=$row['acc'];?></td>
+        <td><?=$row['pw'];?></td>
+        <td><?=$row['email'];?></td>
+        <td><?=$row['tel'];?></td>
+        <td>
+            <a href="edit.php">編輯</a>
+            <a href="del.php?id=<?=$row['id'];?>">刪除</a>
+        </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+<?php
+
+}
+?>
+</table>
+
+
+
+
 </body>
 </html>
