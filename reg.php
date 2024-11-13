d7c1a7908aa2de04a56f09ab5f270b7181b7247d
sandy0v0 <be0324ss@gmail.com>
2024-11-13 13:23:59 +0800

    建立資料庫及資料表+建立SQL語法環境,完成註冊會員功能

--- reg.php
+++ reg.php
@@ -4,13 +4,18 @@
 // print_r($_POST);
 // echo "</pre>";
 
-$sql="insert into `member`(`acc`,`pw`,`email`,`tel`) values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['email']}','{$_POST['tel']}')";
+$sql="insert into `member`(`acc`,`pw`,`email`,`tel`) 
+                    values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['email']}','{$_POST['tel']}')";
 $dsn="mysql:host=localhost;charset=utf8;dbname=crud";
 $pdo=new PDO($dsn,'root','');
 
 if($pdo->exec($sql)){
+
+    // echo "新增資料成功";
     header("location:reg_form.php?status=1");
 }else{
+
+    // echo "新增資料失敗";
     header("location:reg_form.php?status=0");
 }
 
