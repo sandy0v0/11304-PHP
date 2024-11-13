9a203f4142e0295183887c3e082f4030f23d188f
sandy0v0 <be0324ss@gmail.com>
2024-11-13 14:31:08 +0800

    完成資料庫登入功能

--- check_acc.php
+++ check_acc.php
@@ -1,10 +1,21 @@
 <?php 
+// session_start();
+if(!isset($_POST['acc'])){
+    header("location:login.php");
+    exit();
+}
 
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
-if($acc=='admin' && $pw=='123456'){
+$row=$pdo->query("select * from `member` where acc=$acc && pw =$pw")->fetch();
+
+if($acc==$row['acc'] && $pw==$row['pw']){
     echo "帳密正確：登入成功";
+    // session_start();
+    setcookie("login","$acc",time()+180);
+    echo $_COOKIE['login'];
+    echo "<br><a href='login.php'>回首頁</a>";
 }else{
     echo "帳密錯誤：登入失敗";
 
---
+++ check_acc2.php
@@ -0,0 +1,36 @@
+<?php 
+$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+$pdo=new PDO($dsn,'root','');
+
+
+if(!isset($_POST['acc'])){
+    header("location:login2.php");
+    exit();
+}
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+
+// $sql="select * from `member` where `acc`='$acc && `pw` =`$pw`'";
+$sql="select count(id) from `member` where `acc`='$acc && `pw` =`$pw`'";
+echo $sql;
+$row=$pdo->query($sql)->fetchColumn();
+
+//echo "<pre>";
+//print_r($row);
+//echo "</pre>";
+
+// if($acc=$row['acc'] && $pw==$row['pw']){
+if($row>=1){
+    // echo "帳密正確:登入成功";
+    // $_SESSION['login']=$acc;
+    // echo "<br><a href='login2.php'>回首頁</a>";
+    header("location:success.php");
+}else{
+    // echo "帳密錯誤:登入失敗";
+    header("location:login2.php?err=1");
+}
+
+
+?>
